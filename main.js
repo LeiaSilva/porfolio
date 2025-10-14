b6fbe1f1308f37e2ada8de7022ad01172e6b590f
Leia Silva <leiabelensilva@gmail.com>
2025-10-14 10:25:38 -0300

    seccion inicio terminada y agrego archivos principales de seccion sobre mi

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
+//TYPING DE TITULO
+const text = document.querySelector(".dinamica");
 
-
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Desarrolladora Front-End.";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Estudiante de Ing. en Informática.";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "Diseñadora UX/UI.";
+    }, 8000);
+}
+textLoad();
+setInterval(textLoad , 12000);
