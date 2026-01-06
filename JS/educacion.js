9751d94ac385dfd05e15bf0542443569b77a7d96
Leia Silva <leiabelensilva@gmail.com>
2026-01-06 17:44:23 -0300

    Termine carrusel de academia y agregue transiciones de scroll de pantalla, y genere basicos de html para proyectos

--- JS/educacion.js
+++ JS/educacion.js
@@ -1,3 +1,32 @@
-const carruselTrack = document.getElementById('educacion-carrusel-track');
-const totalCards = carruselTrack.children.length;
+//Movimiento de las cards
+const cardBox = document.querySelector('.educacion-container-cardBox');
+const btnNext = document.querySelector('ion-icon[name="chevron-forward-outline"]');
+const btnBack = document.querySelector('ion-icon[name="chevron-back-outline"]');
+const cards = document.querySelectorAll('.educacion-container-card');
 
+let currentIndex = 0;
+
+function updateCarousel() {
+    const cardWidth = cards[0].clientWidth + 48; // El 48 es por los márgenes (1.5rem + 1.5rem aprox)
+    cardBox.style.transform = `translateX(${-currentIndex * cardWidth}px)`;
+}
+
+btnNext.addEventListener('click', () => {
+    if (currentIndex < cards.length - 1) {
+        currentIndex++;
+        updateCarousel();
+    } else {
+        currentIndex = 0;
+        updateCarousel();
+    }
+});
+
+btnBack.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        updateCarousel();
+    } else {
+        currentIndex = cards.length - 1;
+        updateCarousel();
+    }
+});
--- main.js
+++ main.js
@@ -15,4 +15,21 @@ const textLoad = () => {
 textLoad();
 setInterval(textLoad , 12000);
 
+//PROGRESO DE BARRA Y TITULOS EN MOVIMIENTOS
+document.addEventListener("DOMContentLoaded" , () =>{
+    const observar = new IntersectionObserver((entries) =>{
+        entries.forEach(entry =>{
+            const el = entry.target
+            if(entry.isIntersecting)
+            {
+                const valorFinal = el.getAttribute('data-progreso');
+                el.style.setProperty('width', valorFinal, 'important');
 
+                observar.unobserve(el);
+            }
+        });
+    } , {threshold:0.5});
+
+    const elementos =document.querySelectorAll('.animar-ancho');
+    elementos.forEach(el => observar.observe(el));
+})
